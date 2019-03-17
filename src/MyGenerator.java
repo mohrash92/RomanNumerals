514f77a7984bb97dcbbd8f7f614fdb6842994685
Mohammed Rashid <ge_rashid@hotmail.co.uk>
2019-03-17 13:32:24 +0000

    make it pass (use hashmap and convert to switch statement)

--- src/MyGenerator.java
+++ src/MyGenerator.java
@@ -1,15 +1,32 @@
-public class MyGenerator implements RomanNumeralGenerator{
-    public String generate(int number) {
-        String romanNumeral = "";
+import java.util.HashMap;
 
-        if (number <= 3) {
-            for (int i = 0; i < number; i++) {
-                romanNumeral += 'I';
-            }
-        } else {
-            romanNumeral = "IV";
+public class MyGenerator implements RomanNumeralGenerator{
+
+    HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+
+    public String generate(int number) {
+        hmap.put(4, "IV");
+        hmap.put(5, "V");
+
+        String romanNumeral = "";
+        switch (number) {
+            case 1:
+            case 2:
+            case 3:
+                return concatString(number, romanNumeral);
+            case 4:
+                return hmap.get(4);
+            case 5:
+                return hmap.get(5);
         }
 
+        return romanNumeral;
+    }
+
+    public String concatString(int number, String romanNumeral) {
+        for (int i = 0; i < number; i++) {
+            romanNumeral += 'I';
+        }
         return romanNumeral;
     }
 }
