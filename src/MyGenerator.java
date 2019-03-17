fa384365001e48cc4e7f98fe218df2e2d6293b56
Mohammed Rashid <ge_rashid@hotmail.co.uk>
2019-03-17 03:48:12 +0000

    make it pass

--- src/MyGenerator.java
+++ src/MyGenerator.java
@@ -1,6 +1,13 @@
 public class MyGenerator implements RomanNumeralGenerator{
     public String generate(int number) {
-        return "I";
+        String romanNumeral = "";
+        if (number == 1) {
+            romanNumeral = "I";
+        } else if (number == 2) {
+            romanNumeral = "II";
+        }
+
+        return romanNumeral;
     }
 
 }
