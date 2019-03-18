a41524c52ea21d31876f92d1109ce0cb767a9ef1
Mohammed Rashid <ge_rashid@hotmail.co.uk>
2019-03-18 22:47:35 +0000

    make it pass

--- src/MyGenerator.java
+++ src/MyGenerator.java
@@ -1,10 +1,6 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Stream;
+import java.util.*;
 
-public class MyGenerator implements RomanNumeralGenerator{
+public class MyGenerator implements RomanNumeralGenerator {
 
     HashMap<Integer, String> hmap = new HashMap<Integer, String>();
 
@@ -18,24 +14,18 @@ public class MyGenerator implements RomanNumeralGenerator{
         if (hmap.containsKey(number)) {
             return hmap.get(number);
         }
-        String numeral = "";
+
         ArrayList<Integer> numeralSet = new ArrayList<Integer>();
 
         for (Map.Entry<Integer, String> entry : hmap.entrySet()) {
             Integer key = entry.getKey();
-//            String value = entry.getValue();
 
             if (key < number) {
                 numeralSet.add(key);
             }
         }
+        int lastHighest = Collections.max(numeralSet);
 
-        System.out.println(numeral);
-        for(int i = 0; i < number; i++) {
-            numeral += 'I';
-        }
-
-//        System.out.println(number);
-        return numeral;
+        return hmap.get(lastHighest) + generate(number - lastHighest);
     }
 }
