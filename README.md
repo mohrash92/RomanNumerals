8e7471e184e278e860b17aa1e66e9297c109f4d2
Mohammed Rashid <ge_rashid@hotmail.co.uk>
2019-03-18 08:33:21 +0000

    some code refactoring and add maven

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Please explain your approach, assumptions made, or caveats to your solution and add these to the email.
 
 - Created basic set up of Java project with basic unit test to ensure class exists
--
+- refactored tests to use parameterized tests to avoid test duplication
+- refactored code to use hash
--- src/MyGenerator.java
+++ src/MyGenerator.java
@@ -1,32 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class MyGenerator implements RomanNumeralGenerator{
 
     HashMap<Integer, String> hmap = new HashMap<Integer, String>();
 
     public String generate(int number) {
+        hmap.put(1, "I");
         hmap.put(4, "IV");
         hmap.put(5, "V");
+        hmap.put(9, "IX");
+        hmap.put(10, "X");
 
-        String romanNumeral = "";
-        switch (number) {
-            case 1:
-            case 2:
-            case 3:
-                return concatString(number, romanNumeral);
-            case 4:
-                return hmap.get(4);
-            case 5:
-                return hmap.get(5);
+        if (hmap.containsKey(number)) {
+            return hmap.get(number);
+        }
+        String numeral = "";
+        ArrayList<Integer> numeralSet = new ArrayList<Integer>();
+
+        for (Map.Entry<Integer, String> entry : hmap.entrySet()) {
+            Integer key = entry.getKey();
+//            String value = entry.getValue();
+
+            if (key < number) {
+                numeralSet.add(key);
+            }
         }
 
-        return romanNumeral;
-    }
-
-    public String concatString(int number, String romanNumeral) {
-        for (int i = 0; i < number; i++) {
-            romanNumeral += 'I';
+        System.out.println(numeral);
+        for(int i = 0; i < number; i++) {
+            numeral += 'I';
         }
-        return romanNumeral;
+
+//        System.out.println(number);
+        return numeral;
     }
 }
