a08efd1a0025c25cfb4c03284afaafd846c5e949
Mohammed Rashid <ge_rashid@hotmail.co.uk>
2019-03-18 23:52:54 +0000

    make it pass

--- src/MyGenerator.java
+++ src/MyGenerator.java
@@ -12,6 +12,14 @@ public class MyGenerator implements RomanNumeralGenerator {
         hmap.put(5, "V");
         hmap.put(9, "IX");
         hmap.put(10, "X");
+        hmap.put(40, "XL");
+        hmap.put(50, "L");
+        hmap.put(90, "XC");
+        hmap.put(100, "C");
+        hmap.put(400, "CD");
+        hmap.put(500, "D");
+        hmap.put(900, "CM");
+        hmap.put(1000, "M");
 
         if (hmap.containsKey(number)) {
             return hmap.get(number);
