3267d3269288e6dd166273f82045a5bf7224be59
Mohammed Rashid <ge_rashid@hotmail.co.uk>
2019-03-18 23:50:30 +0000

    write test for checking for all mapped values which will be returned

--- test/MyGeneratorTest.java
+++ test/MyGeneratorTest.java
@@ -11,16 +11,38 @@ class MyGeneratorTest {
 
     private static Collection<Object[]> provideIntegersForMyGenerator() {
         return Arrays.asList(new Object[][]{
-                {1, "I"},
-                {2, "II"},
-                {3, "III"},
-                {4, "IV"},
-                {5, "V"},
-                {6, "VI"},
-                {7, "VII"},
-                {8, "VIII"},
-                {9, "IX"},
-                {10, "X"}
+
+            {2, "II"},
+            {3, "III"},
+            {6, "VI"},
+            {7, "VII"},
+            {8, "VIII"},
+            {11, "XI"},
+            {12, "XII"},
+            {13, "XIII"},
+            {14, "XIV"},
+            {15, "XV"},
+            {16, "XVI"},
+            {17, "XVII"},
+            {18, "XVIII"},
+            {19, "XIX"},
+            {20, "XX"},
+
+            // Mapped Values
+            {1, "I"},
+            {4, "IV"},
+            {5, "V"},
+            {9, "IX"},
+            {10, "X"},
+            {40, "XL"},
+            {50, "L"},
+            {90, "XC"},
+            {100, "C"},
+            {400, "CD"},
+            {500, "D"},
+            {900, "CM"},
+            {1000, "M"},
+            {2000, "MM"}
         });
     }
 
